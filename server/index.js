33b14580d705d7e38e0f919f02012e84425f3ebf
unknown <satpal61106@gmail.com>
2026-04-20 19:30:42 +0530

    code updated

--- server/index.js
+++ server/index.js
@@ -20,8 +20,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://studipal-2.onrender.com",
-      "https://studipal-2.onrender.com/"
+      "https://studipal-2.onrender.com"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"],
